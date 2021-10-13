72fd41ee5249beb274dc450d53b2d73e116c3bc5
Takev,Antonio A.M <a.takev@student.fontys.nl>
2021-10-14 00:19:11 +0200

    Create logic for player swapping within team squad

--- streetSoccerManager-Frontend/street-soccer-manager/src/components/Player.js
+++ streetSoccerManager-Frontend/street-soccer-manager/src/components/Player.js
@@ -1,14 +1,19 @@
 import React, {useState} from "react";
 import { useDrag } from "react-dnd";
 
-function Player({player, position}) {
+function Player({player, position, handleSwap}) {
   
+
+  const handleClick = () => {
+    handleSwap(player);
+  }
+
   return (
     <div className="player">
         {/* <span className="kitNr">{player.kitNr}</span> */}
         
         {/* <div className="overallRating">{player.playerStats.overallRating}</div> */}
-        
+        <button onClick={handleClick}><i className="fas fa-exchange-alt"></i></button>
         <div>
           <span className="fa-stack fa-2x">
             <i className="fas fa-tshirt fa-stack-2x"></i>
