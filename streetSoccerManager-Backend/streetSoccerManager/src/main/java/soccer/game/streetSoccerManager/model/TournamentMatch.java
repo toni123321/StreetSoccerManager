31d5946cff737d06b893f47c01ed5d84ba47e348
Takev,Antonio A.M <tonitakev@gmail.com>
2021-10-19 11:42:56 +0200

    Fix bugs sonarqube

--- streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetSoccerManager/model/TournamentMatch.java
+++ streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetSoccerManager/model/TournamentMatch.java
@@ -8,7 +8,7 @@ public class TournamentMatch extends Match {
     @Setter
     private Tournament tournament;
 
-    public TournamentMatch(int id, Team homeTeam, Team awayTeam, int homeTeamGoals, int awayTeamGoals, Kit homeTeamKit, Kit awayTeamKit, Stadium stadium, String matchStatistic, Tournament tournament) {
+    public TournamentMatch(int id, Team homeTeam, Team awayTeam, int homeTeamGoals, int awayTeamGoals, String matchStatistic, Tournament tournament) {
         super(id, homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, matchStatistic);
         this.tournament = tournament;
     }
