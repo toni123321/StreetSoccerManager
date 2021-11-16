9e1bc503fc5508876a949c7fdcd76931238026d3
Takev,Antonio A.M <tonitakev@gmail.com>
2021-11-16 14:19:13 +0100

    Add logout and save token in SameSite coockie with Lax + update user possible

--- streetSoccerManager-Frontend/street-soccer-manager/src/components/Login.js
+++ streetSoccerManager-Frontend/street-soccer-manager/src/components/Login.js
@@ -5,21 +5,42 @@ import { useHistory } from 'react-router-dom';
 import FrontendUserService from "../services/FrontendUserService";
 import UserService from "../services/UserService";
 import axios from "axios";
+import Cookies from 'universal-cookie';
+import jwt_decode from "jwt-decode";
 
 const Login = () => {
-
+    const cookies = new Cookies();
+ 
     const [isUserLogged, setIsUserLogged] = useState(false);
+    const [loginError, setLoginError] = useState(false);
+
+    useEffect(() => {
+        if(cookies.get('login-token') != null){
+            setIsUserLogged(true);
+        } 
+    }, []);
+
     async function handleLogin(details) {
         try{
+            setLoginError(false);
             const response = await UserService.login(details);
-            console.log(response.status);
+            console.log(response.data.Authorization);
+            
+            const token = response.data.Authorization;
+            const decode_token = jwt_decode(token);
+
+            cookies.set('login-token', token, {sameSite: 'lax'});
+            console.log(cookies.get('login-token'));
             setIsUserLogged(true);
         }
         catch (error) {
-            console.log(error.response.data);
+            setLoginError(true);
         }
-        
-        
+    }
+
+    function handleLogout() {
+        cookies.remove('login-token', {sameSite: 'lax'});
+        setIsUserLogged(false);
     }
 
     return (
@@ -28,11 +49,20 @@ const Login = () => {
             (
             <div className="welcome">
                 <h1>Welcome</h1>
-                <button>Log out</button>
+                <button onClick={handleLogout}>Log out</button>
             </div>
             ) :
             (
+            <>
+            {loginError ? 
+            (
+            <>
+            <h3>Wrong credentials</h3>
+            </>
+            ) 
+            : (<></>)}
             <LoginForm handleLogin={handleLogin}/>
+            </>
             )
             }
             
