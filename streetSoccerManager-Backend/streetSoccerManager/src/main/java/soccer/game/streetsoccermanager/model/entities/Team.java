a6f84d1bc2592b46559e9d62209517313d581c9b
Takev,Antonio A.M <tonitakev@gmail.com>
2021-11-30 23:56:38 +0100

    Add calculated overall rating for team

--- streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/Team.java
+++ streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/Team.java
@@ -3,9 +3,13 @@ package soccer.game.streetsoccermanager.model.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.hibernate.annotations.Formula;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Entity
@@ -37,8 +41,48 @@ public class Team {
     @JsonIgnore
     protected Set<Match> awayTeamMatches;
 
-    @Column(nullable = true)
+    @Transient
     private int rating;
+
+    @PostLoad
+    public void onPostLoad() {
+        this.rating = calcTeamRating();
+    }
+
+    private int calcTeamRating(){
+        int rating = 0;
+        int startingPlayersRating = 0;
+        int reservesRating = 0;
+
+        List<PlayerTeamInfo> startingPlayersTeamInfo = new ArrayList<>();
+        List<PlayerTeamInfo> reservesTeamInfo = new ArrayList<>();
+
+        if(this.getPlayersTeamInfo() != null) {
+            startingPlayersTeamInfo = this.getPlayersTeamInfo().stream().filter(p -> p.getPlayer().getPlayerPositionInfo().isStarting()).collect(Collectors.toList());
+            reservesTeamInfo = this.getPlayersTeamInfo().stream().filter(p -> !p.getPlayer().getPlayerPositionInfo().isStarting()).collect(Collectors.toList());
+        }
+
+        for (PlayerTeamInfo playerTeamInfo: startingPlayersTeamInfo) {
+            startingPlayersRating += playerTeamInfo.getPlayer().getPlayerAdditionalInfo().getPlayerStats().getOverallRating();
+        }
+        for (PlayerTeamInfo playerTeamInfo: reservesTeamInfo) {
+            reservesRating += playerTeamInfo.getPlayer().getPlayerAdditionalInfo().getPlayerStats().getOverallRating();
+        }
+
+        if(!playersTeamInfo.isEmpty() &&
+                !startingPlayersTeamInfo.isEmpty()){
+            startingPlayersRating /= startingPlayersTeamInfo.size();
+        }
+
+        if(!playersTeamInfo.isEmpty() &&
+                !reservesTeamInfo.isEmpty()){
+            reservesRating /= reservesTeamInfo.size();
+        }
+
+        rating = ((int) Math.round((startingPlayersRating * 0.8) + (reservesRating * 0.2)));
+
+        return rating;
+    }
 
     public Team(Long id, String name, Formation formation) {
         this.id = id;
