2d7634167f3d3210f6951ee41c7fd39f824aeb6d
Takev,Antonio A.M <tonitakev@gmail.com>
2021-12-05 21:27:22 +0100

    Fix sonarqube issue plus return equals and hashcode annotation where it has been removed

--- streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/PlayerTeamInfo.java
+++ streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/PlayerTeamInfo.java
@@ -10,8 +10,8 @@ import java.util.Objects;
 @Entity
 @Table(name ="player_team_info")
 @NoArgsConstructor
-@Getter
-@Setter
+@Data
+@EqualsAndHashCode(exclude = {"player"})
 public class PlayerTeamInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -33,19 +33,5 @@ public class PlayerTeamInfo {
     public PlayerTeamInfo(int kitNr, Team team) {
         this.kitNr = kitNr;
         this.team = team;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PlayerTeamInfo)) return false;
-        PlayerTeamInfo that = (PlayerTeamInfo) o;
-        return getKitNr() == that.getKitNr() && getTeam().equals(that.getTeam());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getKitNr(), getTeam());
     }
 }
--- streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/UserEntity.java
+++ streetSoccerManager-Backend/streetSoccerManager/src/main/java/soccer/game/streetsoccermanager/model/entities/UserEntity.java
@@ -23,32 +23,28 @@ public class UserEntity {
     private String lastName;
     @Column(nullable = true)
     private String nickname;
-    @Column(nullable = true)
-    private int points;
     private String role;
 
     @OneToOne(mappedBy = "manager", cascade = CascadeType.ALL)
     @JsonIgnore
     protected CustomTeam team;
 
-    public UserEntity(Long id, String email, String password, String firstName, String lastName, String nickname, String role, int points) {
+    public UserEntity(Long id, String email, String password, String firstName, String lastName, String nickname, String role) {
         this.id = id;
         this.email = email;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.nickname = nickname;
-        this.points = points;
         this.role = role;
     }
 
-    public UserEntity(String email, String password, String firstName, String lastName, String nickname, String role, int points) {
+    public UserEntity(String email, String password, String firstName, String lastName, String nickname, String role) {
         this.email = email;
         this.password = password;
         this.firstName = firstName;
         this.lastName = lastName;
         this.nickname = nickname;
-        this.points = points;
         this.role = role;
     }
 
