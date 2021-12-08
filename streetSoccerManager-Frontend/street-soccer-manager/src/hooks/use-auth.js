89bd54d7bce72c3a36814c2ba47642609dbb78a0
Takev,Antonio A.M <tonitakev@gmail.com>
2021-12-08 21:00:58 +0100

    Log in, log out connected to page auth redirection, game navbar design fix

--- streetSoccerManager-Frontend/street-soccer-manager/src/hooks/use-auth.js
+++ streetSoccerManager-Frontend/street-soccer-manager/src/hooks/use-auth.js
@@ -1,5 +1,9 @@
 import React, { useContext, createContext, useState, useEffect } from "react";
-
+import Cookies from 'universal-cookie';
+import jwt_decode from "jwt-decode";
+import TeamService from "../services/TeamService";
+import UserService from "../services/UserService";
+import { useHistory } from 'react-router-dom';
 
 
 const authContext = createContext();
@@ -18,25 +22,60 @@ export function useAuth() {
 }
 
 export function useProvideAuth() {
-  const [user, setUser] = useState(null);
+  let history = useHistory();
+  const cookies = new Cookies();
+
   const [isUserLogged, setIsUserLogged] = useState(false);
 
   useEffect(() => {
-    // if(cookies.get('login-token') !== undefined){
-    //     setIsUserLogged(true);
-    //     // history.push("/game");
-    // }
-    setIsUserLogged(true);
+    if(cookies.get('login-token') !== undefined){
+        setIsUserLogged(true);
+    }
+    
   }, [])
 
-  const signin = cb => {
-    setIsUserLogged(true);
-    cb();
+  function signin (details, from) {
+    UserService.login(details)
+    .then(response => {
+        console.log(response.data.Authorization);
+        const token = response.data.Authorization;
+        const decode_token = jwt_decode(token);
+
+        cookies.set('login-token', token, {sameSite: 'lax'});
+        console.log(cookies.get('login-token'));
+        setIsUserLogged(true);
+        console.log(decode_token.sub);
+        getUserByEmail(decode_token.sub, token);
+        history.replace(from);  
+        return true;
+    })
+    .catch (error => {
+        return false;
+    })
   };
 
-  const signout = cb => {
+  function getUserByEmail(email, token) {
+      UserService.getUserByEmail(email, token)
+      .then((response) => {
+          getUserTeam(response.data.id, token);
+      }
+      );
+      // const response = await UserService.getUserByEmail(email, token);
+      // console.log(response.data);
+  }
+
+  function getUserTeam(userId, token){
+      TeamService.getTeamByUserId(userId, token)
+      .then((response) => {
+          localStorage.setItem("team", JSON.stringify(response.data));
+      }
+      );
+  }
+
+  function signout() {
+    cookies.remove('login-token', {sameSite: 'lax'});
     setIsUserLogged(false);
-    cb();
+    localStorage.removeItem("team");
   };
 
   return {
