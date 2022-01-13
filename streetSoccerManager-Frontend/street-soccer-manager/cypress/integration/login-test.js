f0f96b26c0a7100d497c02f587bee631e940b466
Takev,Antonio A.M <tonitakev@gmail.com>
2022-01-13 21:00:02 +0100

    New user(admin) in initial config + e2e test for log in as admin and log out

--- streetSoccerManager-Frontend/street-soccer-manager/cypress/integration/login-test.js
+++ streetSoccerManager-Frontend/street-soccer-manager/cypress/integration/login-test.js
@@ -14,9 +14,28 @@ describe('test login', () => {
         })
     })
     
-    it('login with correct credentials', () => {
+    it('login with correct credentials as user', () => {
         cy.get('#email').type("erick@gmail.com");
         cy.get('#password').type("Erick@12345");
+        cy.get('.sign-in-btn').click();
+
+        cy.wait(1000);
+
+        cy.location().should((loc) => { 
+            expect(loc.pathname).to.eq('/account')
+        })
+        cy.get('.profile-avatar').click();
+        cy.get('.logoutNavItem').click();
+        cy.wait(1000);
+
+        cy.location().should((loc) => { 
+            expect(loc.pathname).to.eq('/loginPage')
+        })
+    })
+
+    it('login with correct credentials as an admin', () => {
+        cy.get('#email').type("john@gmail.com");
+        cy.get('#password').type("Johny@123");
         cy.get('.sign-in-btn').click();
 
         cy.wait(1000);
